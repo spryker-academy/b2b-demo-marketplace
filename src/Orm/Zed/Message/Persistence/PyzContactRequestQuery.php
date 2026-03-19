9657c0e39a68f7dc6a172eaf60233cadaa3d4d91
Hidran Arias <hidran.arias@spryker.com>
2026-03-19 19:07:28 +0100

    Adds contact-request basics/contact-request-table-schema/complete branch

---
+++ src/Orm/Zed/Message/Persistence/PyzContactRequestQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\Message\Persistence;
+
+use Orm\Zed\Message\Persistence\Base\PyzContactRequestQuery as BasePyzContactRequestQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'pyz_contact_request' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class PyzContactRequestQuery extends BasePyzContactRequestQuery
+{
+
+}
